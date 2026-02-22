1281ececbdbc09f6accf679a241d3013e63f9a5d
Osei Fordwour | LT Networks <osei.fordwour@lt-n.com>
2026-02-22 17:42:24 +0100

    Add basic password generator script

---
+++ password_generator.py
@@ -0,0 +1,13 @@
+#password_generator.py # type: ignore
+import random
+import string
+
+length = int(input("Enter password length: "))
+
+characters = string.ascii_letters + string.digits + string.punctuation
+password = ""
+
+for i in range(length):
+    password += random.choice(characters)
+
+print("Generated password:", password)
